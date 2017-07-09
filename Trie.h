20f73ca921188c75670e9c830bce213016eea1a6
gujueno <gujueno@users.noreply.github.com>
2017-07-09 13:14:37 -0300

    Add files via upload

---
+++ Trie.h
@@ -0,0 +1,27 @@
+
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+#include "Trie.h"
+
+using namespace std;
+
+
+class Trie {
+public:
+    Trie();
+    ~Trie();
+    void addWord(string s);
+    bool searchWord(string s);
+    void deleteWord(string s);
+    void findRadOccurances(string rad);
+private:
+    Node* root;
+};
+
+
+#endif
